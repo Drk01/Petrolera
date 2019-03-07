11058403622d7ad778cf6f217e50335218507265
Omar <ogcm.kael@hotmail.com>
2019-03-07 00:14:57 -0600

    Making a header and footer for proyect views

---
+++ resources/views/layout/footer.blade.php
@@ -0,0 +1,105 @@
+<footer>
+<div style="border: 10px solid #272829;"></div>
+<div class="container-fluid color4">
+  <div class="container">
+    <div class="row">
+      <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+        <h5 class="colorltf7">Enlace</h5>
+        <div class="footerli_p_no">
+          <ul class="list-group" >
+            <li><a target="_blank" class="colorlf7" href="https://www.gob.mx/accesibilidad">Declaración de Accesibilidad</a></li>
+            <li><a target="_blank" class="colorlf7" href="https://www.gob.mx/privacidad">Aviso de privacidad</a></li>
+            <li><a target="_blank" class="colorlf7" href="https://www.gob.mx/terminos">Términos y Condiciones</a></li>
+            <li><a target="_blank" class="colorlf7" href="http://www.ordenjuridico.gob.mx" target="_blank">Marco Jurídico</a></li>
+            <li><a target="_blank" class="colorlf7" href="http://portaltransparencia.gob.mx" target="_blank">Portal de Obligaciones de Transparencia</a></li>
+            <li><a target="_blank" class="colorlf7" href="https://www.infomex.org.mx/gobiernofederal/home.action" target="_blank">Sistema Infomex</a></li>
+            <li><a target="_blank" class="colorlf7" href="http://inicio.ifai.org.mx/SitePages/ifai.aspx" target="_blank">INAI</a></li>
+            <li><a target="_blank" class="colorlf7" href="https://www.gob.mx/sitemap">Mapa de sitio</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+        <h5 class="colorltf7">¿Qué es gob.mx?</h5>
+        <p  class="colorlf7">Es el portal único de trámites, información y participación ciudadana. <a target="_blank" href="https://www.gob.mx/que-es-gobmx" style="color:white">Leer más</a></p>
+        <div class="footerli_p_no">
+          <ul class="list-group">
+            <li><a target="_blank" class="colorlf7" href="https://www.gob.mx/en/index">English</a></li>
+            <li><a target="_blank" class="colorlf7" href="https://www.gob.mx/temas">Temas</a></li>
+            <li><a target="_blank" class="colorlf7" href="http://reformas.gob.mx" target="_blank">Reformas</a></li>
+          </ul>
+        </div>
+      </div>
+      <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+        <h5 class="colorltf7">Contacto</h5>
+        <p  class="colorlf7">Mesa de ayuda: dudas e información <br> gobmx@funcionpublica.gob.mx</p>
+        <p><a target="_blank" href="https://www.gob.mx/tramites/ficha/presentacion-de-quejas-y-denuncias-en-la-sfp/SFP54"><div class="footer_hover"><span  class="colorlf7">Denuncia contra servidores públicos</span></div></a></p>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+        <label for="email" class="colort2f7">Mantente informado. Suscríbete.</label>
+        <div class="input-group footer_input">
+          <input id="pass-2" type="password" class="form-control" placeholder="usuario@ejemplo.com">
+          <span class="input-group-btn">
+            <button class="btn btn-secondary" type="button" id="show-pass" style="background:rgb(75,144, 226);"><i class="fa fa-chevron-right" aria-hidden="true" style="color:white;"></i></button>
+          </span>
+        </div>
+      </div>
+      <div class="col-4"></div>
+    <div class="col-sm-12 col-md-4 col-lg-4 col-xl-4">
+      <h5 class="colort2f7">Síguenos en</h5>
+      <a target="_blank" href="https://www.facebook.com/gobmx"><i class="fa fa-facebook fa-lg" aria-hidden="true" style="color:white;padding-left:20px;"></i></a>
+      <a target="_blank" href="https://twitter.com/gobmx"><i class="fa fa-twitter fa-lg espacio" aria-hidden="true" style="color:white;"></i></a>
+    </div>
+
+
+    </div>
+  </div>
+</div>
+ <div class="container-fluid color2">
+    <div class="container">
+      <div class="row">
+        <div class="col-4">
+          <img src="../img/gobmxlogo.svg" id="logo2"/>
+        </div>
+        <div class="col-md-offset-4 col-4"></div>
+      </div>
+    </div>
+  </div>
+</footer>
+
+  <script src="{{public_path()}}/js/jquery-3.1.1.min.js"></script>
+  <script src="{{public_path()}}/js/bootstrap.min.js"></script>
+
+
+  <script>
+      /*Para mostrar la contraseña*/
+      $(document).ready(function () {
+        $('#show-pass').click(function () {
+        if ($('#pass-2').attr('type') === 'text') {
+         $('#pass-2').attr('type', 'password');
+        } else {
+         $('#pass-2').attr('type', 'text');
+        }
+         });
+        });
+      </script>
+
+      <script>
+	  	function registro_exitoso(mensaje){
+			$(window).load(function(){
+				$(mensaje).modal("show");
+			});
+			setTimeout(function(){
+				$(mensaje).modal("hide");
+			},2500);
+		}
+      </script>
+      <script>
+      $('#myModal').on('shown.bs.modal', function () {
+        $('#myInput').focus()
+      })
+      </script>
+
+</body>
+</html>
---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <!--Importante-->
+    <link rel="stylesheet" href="../css/Bootstrap4/css/bootstrap.min.css" >
+    <link rel = "stylesheet"  href = "../css/Bootstrap4/font-awesome-4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/estilo_gob.css">
+
+    <!-- Custom styles for this template -->
+    <link href="css/simple-sidebar.css" rel="stylesheet">
+    <link href="css/estilo_formularios.css" rel="stylesheet">
+    <link href="css/contenido.css" rel="stylesheet">
+
+
+  </head>
+  <body>
+    <header>
+      <div class="fixed-top">
+      <nav class="navbar navbar-toggleable-md navbar-light bg-faded navegacion_gob">
+        <div class="container">
+          <a target="_blank" class="navbar-brand" href="https://www.gob.mx"><img src="../img/gobmxlogo.svg" id="logo"/></a>
+          <button class="navbar-toggler navbar-toggler-right"  type="button"  data-toggle="collapse" data-target="#navbargob" aria-controls="navbargob" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa fa-bars toggler_gob" aria-hidden="true"></i>
+          </button>
+          <div style="border-bottom: 2px solid #404343;"></div>
+          <div class="collapse navbar-collapse" id="navbargob">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active"><a target="_blank" class="nav-link hover_gob" href="https://www.gob.mx/tramites" title="Trámites"><h5 class="colorlgob7">Trámites</h5></a></li>
+              <li class="nav-item active"><a target="_blank" class="nav-link hover_gob" href="https://www.gob.mx/gobierno" title="Gobierno"><h5 class="colorlgob7">Gobierno</h5></a></li>
+              <li class="nav-item active"><a target="_blank" class="nav-link hover_gob" href="https://www.gob.mx/participa" title="Participación Ciudadana"><h5 class="colorlgob7">Participa</h5></a></li>
+              <li class="nav-item active"><a target="_blank" class="nav-link hover_gob" href="http://datos.gob.mx" title="Datos Abiertos"><h5 class="colorlgob7">Datos</h5></a></li>
+              <li class="nav-item active"><a target="_blank" target="_blank" class="nav-link hover_gob" href="https://www.gob.mx/busqueda"><span class="sr-only"><h5 class="colorlgob7">Búsqueda</h5></span><i class="fa fa-search buscar"></i></a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+      <nav class="navbar navbar-toggleable-md navbar-light bg-faded navegacion_api sub-navbar">
+        <div class="container">
+          <a target="_blank" target="_blank" class="navbar-brand " href="http://www.puertosalinacruz.com.mx/espi/0000001/inicio"><span class="colorltapi7">API Salina Cruz</span></a>
+          <button class="navbar-toggler navbar-toggler-right"  type="button"  data-toggle="collapse" data-target="#navbarapi" aria-controls="navbarapi" aria-expanded="false" aria-label="Toggle navigation">
+            <i class="fa fa-bars toggler_gob" aria-hidden="true"></i>
+          </button>
+          <div style="border-bottom: 2px solid #404343;"></div>
+          <div class="collapse navbar-collapse" id="navbarapi">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item active"><a target="_blank" target="_blank" class="nav-link hover_gob" href="http://www.puertosalinacruz.com.mx/esps/0000201/quienes-somos" ><h5 class="colorlapi7">Quiénes Somos</h5></a></li>
+              <li class="nav-item active"><a target="_blank" target="_blank" class="nav-link hover_gob" href="http://www.puertosalinacruz.com.mx/esps/0000202/acerca-del-puerto" ><h5 class="colorlapi7">Acerca del Puerto</h5></a></li>
+              <li class="nav-item active"><a target="_blank" target="_blank" class="nav-link hover_gob" href="http://www.puertosalinacruz.com.mx/esps/0000203/infraestructura" ><h5 class="colorlapi7">Infraestructura</h5></a></li>
+              <li class="nav-item active"><a target="_blank" target="_blank" class="nav-link hover_gob" href="http://www.puertosalinacruz.com.mx/esps/0000205/oportunidades-de-negocios"><h5 class="colorlapi7">Oportunidades de Negocios</h5></a></li>
+              <li class="nav-item active"><a target="_blank" target="_blank" class="nav-link hover_gob" href="http://www.puertosalinacruz.com.mx/esps/0000207/tarifas"><h5 class="colorlapi7">Tarifas</h5></a></li>
+              <li class="nav-item active"><a target="_blank" target="_blank" class="nav-link hover_gob" href="http://www.puertosalinacruz.com.mx/esps/2110211/transparencia"><h5 class="colorlapi7">Transparencia</h5></a></li>
+            </ul>
+          </div>
+        </div>
+      </nav>
+    </div>
+  </header>
+
