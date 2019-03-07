de0ba4b893c53c69b724aeff9a9070b8f9f4e20e
Omar <ogcm.kael@hotmail.com>
2019-03-07 00:15:23 -0600

    Making a new index view.

---
+++ resources/views/index.blade.php
@@ -0,0 +1,57 @@
+@include('layout.header');
+
+<div class="container" id="login">
+    <div class="row">
+        <div class="col-12">
+            <p>
+                <center>
+                    <h2>Sistema de inventario</h2>
+                </center>
+            </p><br>
+        </div>
+    </div>
+    <div class="row justify-content-center">
+        <div class="col col-xs-6 col-sm-8 col-md-6 col-xl-6 col-xl-6">
+            <div class="card">
+                <h3 class="card-header special-color-dark text-center" id="cabecera-card">Iniciar Sesión</h3>
+                <div class="card-body">
+                    <div class="card-block">
+                        <div class="text-center"><img src="img/Logo_APISAL.jpg" id="logo_comercializacion"
+                                alt=""></div>
+                        <br><!-- Form login -->
+                        <form action="entrar" method="post">
+                            @csrf
+                            @method("POST")
+                            <div class="row">
+                                <div class="form-group col cabecera-contenido">
+                                    <input type="text" class="form-control cabecera-contenido" placeholder="Usuario"
+                                        name="usuario" autofocus autocomplete="off" />
+                                </div>
+                            </div>
+
+                            <div class="row">
+                                <div class="input-group col ">
+                                    <input id="pass-2" type="password" placeholder="Contraseña" name="password" class="form-control cabecera-contenido"
+                                        autocomplete="off" />
+                                    <span class="input-group-btn show-password">
+                                        <button class="btn btn-default" type="button" id="show-pass">
+                                            <div class="fa fa-eye prefix grey-text"></div>
+                                        </button>
+                                    </span>
+                                </div>
+                            </div>
+                            <p>
+                                <div class="text-center">
+                                    <input type="submit" class="btn boton_negro" name="login" id="btn_login" value="Iniciar">
+                                </div>
+                            </p>
+                        </form>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+
+
+@include('layout.footer');
