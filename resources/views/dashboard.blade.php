955eb166ebadd3cd04bc5e86c9ce66dc4b6d7a78
Omar <ogcm.kael@hotmail.com>
2019-03-08 23:23:58 -0600

    Deleting code to make a template.

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -2,39 +2,17 @@
 
 @include('layout.nav')
 <div class="col-8 titulo_contenido" style="padding-top:30px;">
-        <div class="well">
-          <center><h2>SISTEMA DE ENCUESTAS</h2></center>
-        </div>
-        <div style="padding-top:100px;padding-bottom:100px;">
-        <div id="carouselExampleIndicators" class="carousel slide" data-ride="carousel">
-          <ol class="carousel-indicators">
-            <li data-target="#carouselExampleIndicators" data-slide-to="0" class="active"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="1"></li>
-            <li data-target="#carouselExampleIndicators" data-slide-to="2"></li>
-          </ol>
-          <div class="carousel-inner" role="listbox">
-            <div class="carousel-item active">
-              <img class="d-block img-fluid" src="App/Vista/imagenes/01.jpg" alt="First slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="App/Vista/imagenes/02.jpg" alt="Second slide">
-            </div>
-            <div class="carousel-item">
-              <img class="d-block img-fluid" src="App/Vista/imagenes/03.jpg" alt="Third slide">
-            </div>
-          </div>
-          <a class="carousel-control-prev" href="#carouselExampleIndicators" role="button" data-slide="prev">
-            <span class="carousel-control-prev-icon" aria-hidden="true"></span>
-            <span class="sr-only">Previous</span>
-          </a>
-          <a class="carousel-control-next" href="#carouselExampleIndicators" role="button" data-slide="next">
-            <span class="carousel-control-next-icon" aria-hidden="true"></span>
-            <span class="sr-only">Next</span>
-          </a>
-        </div>
-        </div>
-      </div> <div class="col-2" style="background:#333;">
-      </div>
+    <div class="well">
+        <center>
+            <h2>SISTEMA DE INVENTARIO</h2>
+        </center>
     </div>
-    
+    <div style="padding-top:100px;padding-bottom:100px;">
+      {{-- Body --}}
+    </div>
+</div>
+<div class="col-2" style="background:#333;">
+</div>
+</div>
+
 @include('layout.footer')
