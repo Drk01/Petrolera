d4d2bb2a577cdb696e78df7395f4028053a9ba78
Omar <ogcm.kael@hotmail.com>
2019-03-12 14:05:47 -0600

    Unnecesary dependency removed

--- app/Http/Controllers/AlmacenController.php
+++ app/Http/Controllers/AlmacenController.php
@@ -5,7 +5,6 @@ namespace App\Http\Controllers;
 use Illuminate\Support\Facades\Auth;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
-use Illuminate\Support\Facades\Storage;
 
 class AlmacenController extends Controller
 {
