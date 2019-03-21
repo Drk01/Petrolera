14ff83f892b3c24ea213a89d4f743a597a46bb12
Omar Monrroy <ogcm.kael@hotmail.com>
2019-03-20 18:39:19 -0600

    Changing LoginController to add IsAdmin Middleware in necesary controllers

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -5,33 +5,22 @@ namespace App\Http\Controllers\Auth;
 use App\Http\Controllers\Controller;
 use Illuminate\Support\Facades\Auth;
 use DB;
+
 class LoginController extends Controller
 {
-    public function login(){
+    public function login()
+    {
 
-       $credentials = $this->validate(request(),[
+        $credentials = $this->validate(request(), [
             'user' => 'required|string',
             'password' => 'required|string'
         ]);
 
-        //Comprobar si los datos del usuario coinciden en la bd y si es administrador
-        $userData = DB::table('users')->select('id')->where('user','=',request('user'))->first();
-
-        if(empty($userData)){
-            return back()->withErrors(['email' => trans('auth.failed')])->withInput(request(['user']));
+        if (Auth::attempt($credentials)) {
+            return redirect()->route('dashboard');
         }
 
-        $userID = $userData->id;
-
-        $isAdmin = DB::table('users_roles')->select('id')->where('users_id','=',$userID)->where('roles_id','=','1')->first();
-
-        if(!empty($isAdmin)){
-            if (Auth::attempt($credentials)) {
-                return redirect()->route('dashboard');
-            }
-        }
-
-        return back()->withErrors(['email'=> trans('auth.failed')])->withInput(request(['user']));
+        return back()->withErrors(['email' => trans('auth.failed')])->withInput(request(['user']));
     }
 
     public function showLoginForm()
