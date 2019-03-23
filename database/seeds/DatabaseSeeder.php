a04636b917e6ce7c270180f359e94a16df6d7de9
Drk01 <ogcm.kael@hotmail.com>
2019-03-23 02:56:02 -0600

    Reorder seeders

--- database/seeds/DatabaseSeeder.php
+++ database/seeds/DatabaseSeeder.php
@@ -15,7 +15,7 @@ class DatabaseSeeder extends Seeder
         $this->call(WorkareasTableSeeder::class);
         $this->call(RolesTableSeeder::class);
         $this->call(UsersTableSeeder::class);
-        $this->call(StorageTypeSeeder::class);
         $this->call(loanStatusTableSeeder::class);
+        $this->call(StorageTypeSeeder::class);
     }
 }
