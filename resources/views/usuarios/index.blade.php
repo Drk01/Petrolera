0e8b3908845a56a8700feac95ced9a5d9109bcd2
Drk01 <ogcm.kael@hotmail.com>
2019-03-31 00:27:42 -0600

    Reordered option buttons

--- resources/views/usuarios/index.blade.php
+++ resources/views/usuarios/index.blade.php
@@ -26,16 +26,17 @@ Lista de usuarios
 
         <td>
             <center>
-                <form action="{{ route('usuarios.destroy',$Usuario->id) }}" method="POST" style="display: inline-block;">
-                    @csrf
-                    @method('DELETE')
-                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash"
-                            aria-hidden="true"></i></button>
-                </form>
                 <form action="{{ route('usuarios.edit',$Usuario->id) }}" method="get" style="display: inline-block">
                     @csrf
                     <button style="display: inline-block; margin-left: 1px;" type="submit"
                         class="btn btn-info btn-sm"><i class="fa fa-pencil-square-o" aria-hidden="true"></i></button>
+                </form>
+                <form action="{{ route('usuarios.destroy',$Usuario->id) }}" method="POST"
+                    style="display: inline-block;">
+                    @csrf
+                    @method('DELETE')
+                    <button type="submit" class="btn btn-danger btn-sm"><i class="fa fa-trash"
+                            aria-hidden="true"></i></button>
                 </form>
             </center>
         </td>
