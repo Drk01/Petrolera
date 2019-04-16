0247d0eb1fbd9e14284a9d2b093a67db31fa28e8
Omar <ogcm.kael@hotmail.com>
2019-04-16 02:21:44 -0500

    App name assigned to html title

--- resources/views/layout/header.blade.php
+++ resources/views/layout/header.blade.php
@@ -4,6 +4,7 @@
 <head>
     <!-- Required meta tags -->
     <meta charset="utf-8">
+    <title>{{ config('app.name') }}</title>
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
 
     <!-- Bootstrap CSS -->
