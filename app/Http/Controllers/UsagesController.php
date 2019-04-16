cf6a16a6880e624380e325de8d486067379c55d3
Omar <ogcm.kael@hotmail.com>
2019-04-16 04:33:39 -0500

    CRUD Usos

--- app/Http/Controllers/UsagesController.php
+++ app/Http/Controllers/UsagesController.php
@@ -3,9 +3,16 @@
 namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
+use Illuminate\Support\Facades\DB;
 
 class UsagesController extends Controller
 {
+
+    public function __construct()
+    {
+        $this->middleware('auth');
+        $this->middleware('isAdmin')->only('destroy');
+    }
     /**
      * Display a listing of the resource.
      *
@@ -13,7 +20,11 @@ class UsagesController extends Controller
      */
     public function index()
     {
-        //
+        $Usages = DB::table('usage')->get();
+
+        return view('usages.index')->with([
+            'usos' => $Usages
+        ]);
     }
 
     /**
@@ -23,7 +34,9 @@ class UsagesController extends Controller
      */
     public function create()
     {
-        //
+        return view('usages.create')->with([
+            'Accion' => 'Crear'
+        ]);
     }
 
     /**
@@ -34,7 +47,19 @@ class UsagesController extends Controller
      */
     public function store(Request $request)
     {
-        //
+        $Validate = $request->validate([
+             'name' => 'required',
+             'description' => 'required'
+        ]);
+
+        DB::table('usage')->insert([
+            'name' => trim($request->name),
+            'description' => trim($request->description),
+            'created_at' => now(),
+            'updated_at' => now()
+        ]);
+
+        return redirect(route('usos.index'));
     }
 
     /**
@@ -45,7 +70,12 @@ class UsagesController extends Controller
      */
     public function show($id)
     {
-        //
+        $Uso = DB::table('usage')->where('id',$id)->first();
+
+        return view('usages.create')->with([
+            'uso' => $Uso,
+            'Accion' => 'Mostrar'
+        ]);
     }
 
     /**
@@ -56,7 +86,12 @@ class UsagesController extends Controller
      */
     public function edit($id)
     {
-        //
+        $uso = DB::table('usage')->where('id',$id)->first();
+
+        return view('usages.create')->with([
+            'uso' => $uso,
+            'Accion' => 'Editar'
+        ]);
     }
 
     /**
@@ -68,7 +103,17 @@ class UsagesController extends Controller
      */
     public function update(Request $request, $id)
     {
-        //
+        $Validate = $request->validate([
+            'name' => ['required']
+        ]);
+
+        DB::table('usage')->where('id',$id)->update([
+            'name' => trim($request->name),
+            'description' => trim($request->description),
+            'updated_at' => now()
+        ]);
+
+        return redirect(route('usos.index'));
     }
 
     /**
@@ -79,6 +124,12 @@ class UsagesController extends Controller
      */
     public function destroy($id)
     {
-        //
+        $Usage = DB::table('storage_usage')->where('usage_id','=',$id)->count();
+        if($Usage==0){
+            DB::table('usage')->where('id','=',$id)->delete();
+            return redirect()->back();
+        }else{
+            return redirect()->back()->withErrors(['No puedes eliminar una marca que tenga productos dentro']);
+        }
     }
 }
---
+++ resources/views/usages/create.blade.php
@@ -0,0 +1,51 @@
+@section('Encabezado')
+{{$Accion}} uso
+@endsection
+
+@include('layout.header')
+<center>
+<div class="btn-group">
+    <button class="btn btn-success" onclick="window.location.href='{{ route('usos.index') }}'">Regresar al menú</button>
+</div>
+</center>
+
+@if ($Accion != 'Mostrar')
+@if ($Accion == 'Editar')
+<form action="{{ route('usos.update',$uso->id) }}" method="post" autocomplete="off">
+    @method('PUT')
+@else
+<form action="{{ route('usos.store') }}" method="post" autocomplete="off">
+
+@endif
+@csrf
+    @endif
+
+    <div class="form-group">
+        <label for="name">Nombre: </label>
+        <input required type="text" name="name" id="name" class="form-control" @if ($Accion !='Crear' )
+            value="{{ $uso->name }}" @if ($Accion=='Mostrar' ) disabled @endif @endif>
+    </div>
+    <div class="form-group">
+        <label for="description">Descripción: </label>
+        <textarea required class="form-control" name="description" id="description" rows="3" @if ($Accion=='Mostrar' )
+            disabled @endif>@if ($Accion != 'Crear'){{ $uso->description }}@endif</textarea>
+    </div>
+
+    @if ($Accion != 'Mostrar')
+    @if ($Accion == 'Editar')
+    <button type="submit" class="btn btn-success btn-lg" style="float:right">Editar</button>
+    @else
+    <button type="submit" class="btn btn-success btn-lg" style="float:right">Crear</button>
+    @endif
+</form>
+@endif
+
+
+
+
+
+
+
+
+
+@include('layout.footer')
