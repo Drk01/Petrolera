80f7d202a5e15acae7a6ea17b51692a2482bf691
Omar Monrroy <ogcm.kael@hotmail.com>
2019-06-09 01:16:59 -0500

    Create users() method in Workarea.
    Workarea belongsToMany users()

--- app/Workarea.php
+++ app/Workarea.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Workarea extends Model
 {
-    //
+    public function users()
+    {
+        return $this->belongsToMany('App\User')->withTimestamps();
+    }
 }
