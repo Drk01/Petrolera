010e8e10349b079876ca075c2934237418aecc42
Omar Monrroy <ogcm.kael@hotmail.com>
2019-06-14 05:18:18 -0500

    belongsToMany environment_storage M:M

--- app/Environment.php
+++ app/Environment.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Environment extends Model
 {
-
+    public function storages()
+    {
+        return $this->belongsToMany('App\Storage')->withTimestamps();
+    }
 }
--- app/Storage.php
+++ app/Storage.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Storage extends Model
 {
-    //
+    public function environments()
+    {
+        return $this->belongsToMany('App\Environments')->withTimestamps();
+    }
 }
