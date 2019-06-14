624eda18ef128793d5b8f7fbeb847c4f74c2f5f6
Omar Monrroy <ogcm.kael@hotmail.com>
2019-06-14 05:33:24 -0500

    Create DriveType_Storage pivot table.
    Create pivot table for DriveType_Storage M:M relationship.

---
+++ database/migrations/2019_06_14_103030_create__drive_type__storage_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateDriveTypeStorageTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('DriveType_Storage', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->integer('DriveType_id')->unsigned();
+            $table->integer('Storage_id')->unsigned();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('DriveType_Storage');
+    }
+}
