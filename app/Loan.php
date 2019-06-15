c1c10388d1718ddbcacbc155d6e293c9fbcd49fb
Omar Monrroy <ogcm.kael@hotmail.com>
2019-06-15 07:28:13 -0500

    Create Loan_User M:1 ORM relationship

--- app/Loan.php
+++ app/Loan.php
@@ -6,5 +6,9 @@ use Illuminate\Database\Eloquent\Model;
 
 class Loan extends Model
 {
-    //
+
+    public function user()
+    {
+        return $this->belongsTo('App\User');
+    }
 }
--- app/User.php
+++ app/User.php
@@ -46,4 +46,9 @@ class User extends Authenticatable
     {
         return $this->belongsToMany('App\Workarea')->withTimestamps();
     }
+
+    public function loans()
+    {
+        return $this->hasMany('App\Loan');
+    }
 }
