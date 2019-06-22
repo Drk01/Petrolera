8a56a02a8846867571bb43cd80f16f0d8c3fa979
Omar Monrroy <ogcm.kael@hotmail.com>
2019-06-22 06:34:46 -0500

    Fixed 'almacen.index' view

--- resources/views/almacen/index.blade.php
+++ resources/views/almacen/index.blade.php
@@ -12,54 +12,33 @@ Listado de artículos
         <tr>
             <th>Nombre del artículo </th>
             <th>Descripción</th>
-            <th>Ubicación</th>
-            <th>Resposable</th>
+            <th>Existencias</th>
             <th></th>
         </tr>
     </thead>
     <tbody>
-        @foreach ($storage as $l1 => $articulo)
+        @foreach ($storages as $l1 => $articulo)
         <tr>
-            <td>{{ $articulo->productName }}</td>
+            <td>{{ $articulo->name }}</td>
             <td>{{ $articulo->description }}</td>
             <td>
-                @foreach ($storage_ubication as $l2 => $su)
-                    @if($su->storage_id == $articulo->id)
-                            @foreach ($ubications as $l3 => $ubication)
-                                @if ($ubication->id == $su->ubication_id)
-                                    {{ $ubication->name }}
-                                    @break
-                                @endif
-                            @endforeach
-                        @break
-                    @endif
-                @endforeach
-            </td>
-            <td>
-                @foreach ($responsibles as $l2 => $r)
-                    @if($r->storage_id == $articulo->id)
-                            @foreach ($users as $l3 => $user)
-                                @if ($user->id == $r->user_id)
-                                    {{ $user->name }}
-                                    @break
-                                @endif
-                            @endforeach
-                        @break
-                    @endif
-                @endforeach
+                {{ $articulo->stocks->count() }}
             </td>
             <td>
                 <center>
-                    <form style="display: inline-block" action="{{ route('almacen.show',$articulo->id) }}" method="get">
-                        @csrf
-
-                        <button type="submit" class="btn btn-outline-info btn-block">Mostrar mas...</button>
+                    <button type="button" onclick="document.getElementById('almacen{{ $articulo->id }}').submit()" class="btn btn-outline-info btn-block">Mostrar mas...</button>
                 </center>
             </td>
         </tr>
         @endforeach
     </tbody>
 </table>
+
+@foreach ($storages as $item)
+    <form id="almacen{{ $item->id }}" action="{{ route('almacen.show',$item->id) }}" method="get">@csrf</form>
+@endforeach
+
+
 
 
 @include('layout.footer')
