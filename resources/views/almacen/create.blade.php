3cc0e2c731bc6522d60d4046114a80e3921427b2
Omar Monrroy <ogcm.kael@hotmail.com>
2019-06-22 06:34:06 -0500

    Code block for 'AlmacenController.show'

--- resources/views/almacen/create.blade.php
+++ resources/views/almacen/create.blade.php
@@ -98,11 +98,90 @@
   </form>
 @endif
 
+@if ($Accion == 'Mostrar')
+
+      <div class="form-group">
+          <label for="productName">Nombre del producto: </label>
+          <input disabled required type="text" name="productName" class="form-control" id="productName"
+              placeholder="Inserte el nombre del producto" value="{{ $storage->name }}">
+      </div>
+      <div class="form-group">
+          <label for="description">Descripción del producto: </label>
+          <textarea disabled required class="form-control" name="description" id="description" rows="3"
+              placeholder="Inserte una descripción sobre el producto">{{ $storage->description }}</textarea>
+      </div>
+      <div class="form-group">
+          <label for="observations">Observaciones: </label>
+          <textarea disabled placeholder="Este producto no tiene observaciones" class="form-control" name="observations"
+              id="observations" rows="3">{{ $storage->observations }}</textarea>
+      </div>
+
+      <div class="form-group">
+          <div class="form-row">
+              <div class="col">
+                  <label for="trademark">Marca: </label>
+                  <select disabled required class="form-control" name="trademark" id="trademark">
+                    @foreach ($storage->trademarks as $element)
+                      <option selected>{{ $element->name }}</option>
                     @endforeach
+                  </select>
+              </div>
+              <div class="col">
+                  <label for="driveType">Categoría: </label>
+                  <select disabled required multiple class="form-control" name="driveType[]" id="driveType">
+                    @foreach ($storage->driveTypes as $element)
+                      <option selected>{{ $element->name }}</option>
                     @endforeach
+                  </select>
+              </div>
+          </div>
+      </div>
+      <div class="form-group">
+          <div class="form-row">
+              <div class="col">
+                  <label for="cunit">Contenido: </label>
+                  <input disabled required class="form-control" type="number" min="1" name="cunit" id="cunit" value="{{ $storage->units->first()->pivot->size }}">
+              </div>
+              <div class="col">
+                  <label for="units">Unidad de medida: </label>
+                  <select disabled required class="custom-select form-control" name="units" id="units">
+                    @foreach ($storage->units as $element)
+                      <option selected>{{ $element->name }}</option>
                     @endforeach
+                  </select>
+              </div>
+          </div>
+      </div>
+      <div class="form-group">
+          <label for="enviroment">Precauciones ambientales: </label>
+          <select disabled required name="enviroment[]" id="enviroment" class="form-control" multiple>
+            @foreach ($storage->environments as $element)
+              <option selected>{{ $element->name }}</option>
             @endforeach
+          </select>
+      </div>
+      <div class="form-group">
+          <div class="form-row">
+              <div class="col">
+                  <label for="usage">Usos: </label>
+                  <select disabled required class="form-control" multiple name="uses" id="uses">
+                    @foreach ($storage->usages as $element)
+                      <option selected>{{ $element->name }}</option>
                     @endforeach
+                  </select>
+              </div>
+              <div class="col">
+                  <label for="trashtype">Tipo de residuo: </label>
+                      <select disabled required class="form-control" name="trashtype" multiple id="trashtype">
+                        @foreach ($storage->trashTypes as $element)
+                          <option selected>{{ $element->name }}</option>
+                        @endforeach
+                  </select>
+              </div>
+          </div>
+      </div>
+
+@endif
 
 <script type="text/javascript">
     $(document).ready(function () {
