5493d3d6faaada1264e8afe6402b2e16cc0d8014
Omar Monrroy <ogcm.kael@hotmail.com>
2019-06-22 18:39:36 -0500

    Fixed middleware 'CheckIfAuthUserIsAdmin'

--- app/Http/Middleware/CheckIfAuthUserIsAdmin.php
+++ app/Http/Middleware/CheckIfAuthUserIsAdmin.php
@@ -3,7 +3,8 @@
 namespace App\Http\Middleware;
 
 use Closure;
-use DB;
+use app\User;
+use app\Role;
 
 class CheckIfAuthUserIsAdmin
 {
@@ -16,12 +17,9 @@ class CheckIfAuthUserIsAdmin
      */
     public function handle($request, Closure $next)
     {
-        $UserID = Auth()->user()->id;
-        $roles_id = DB::table('users_roles')->select(['roles_id'])
-        ->where('users_id','=',$UserID)
-        ->where('roles_id','=','1')->first();
+        $userRole = User::where('id',auth()->user()->id)->first()->roles()->get();
 
-        if($roles_id){
+        if($userRole->contains(Role::where('name','Administrador')->first())){
             return $next($request);
         }
         return redirect()->back();
